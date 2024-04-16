8145e069c171bb6928667e8735a6c4001cada603
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-04-16 15:44:31 +0200

    :rocket: Populate icon library

---
+++ components/uikit/icons/arrows/IconArrowRight.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <svg class="icon" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M21 12.5L16 7.5M21 12.5L16 17.5M21 12.5H3" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+</template>
+
+<style scoped lang="sass">
+
+</style>
---
+++ components/uikit/icons/arrows/IconChevronRight.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <svg class="icon" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M12.375 10.125L15.75 13.5L12.375 16.875" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+</template>
+
+<style scoped lang="sass">
+
+</style>
