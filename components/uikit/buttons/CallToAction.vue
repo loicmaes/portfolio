704fbee89e72eb5da407d1e0531eeb71f2cb4896
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-05-02 12:57:33 +0200

    :rocket: Add new option to CTA component (static type)

--- components/uikit/buttons/CallToAction.vue
+++ components/uikit/buttons/CallToAction.vue
@@ -1,21 +1,22 @@
 <script setup lang="ts">
-import {CtaIconPosition, type CtaVariant} from "assets/types/callToAction";
+import {CtaIconPosition, CtaStaticTheme, type CtaVariant} from "assets/types/callToAction";
 
 type Props = {
   to?: string,
   variant?: CtaVariant,
   disabled?: boolean,
   submit?: boolean,
-  iconPosition?: CtaIconPosition
+  iconPosition?: CtaIconPosition,
+  static?: CtaStaticTheme
 };
 
 defineProps<Props>();
 </script>
 
 <template>
-  <NuxtLink :to="to" class="button" :data-icon-position="iconPosition ?? 'none'" :data-variant="variant ?? 'primary'" v-if="to"><slot /></NuxtLink>
-  <a :href="to" class="button" :data-icon-position="iconPosition ?? 'non'" :data-variant="variant ?? 'primary'" :target="to.startsWith('http') ? '_blank' : '_self'" v-else-if="to && (to.startsWith('mailto') || to.startsWith('tel') || to.startsWith('http'))"><slot /></a>
-  <button class="button" :data-icon-position="iconPosition ?? 'none'" :data-variant="variant ?? 'primary'" :type="submit ? 'submit' : 'button'" v-else><slot /></button>
+  <NuxtLink :to="to" class="button" :class="`static-${static}`" :data-icon-position="iconPosition ?? 'none'" :data-variant="variant ?? 'primary'" v-if="to"><slot /></NuxtLink>
+  <a :href="to" class="button" :class="`static-${static}`" :data-icon-position="iconPosition ?? 'non'" :data-variant="variant ?? 'primary'" :target="to.startsWith('http') ? '_blank' : '_self'" v-else-if="to && (to.startsWith('mailto') || to.startsWith('tel') || to.startsWith('http'))"><slot /></a>
+  <button class="button" :class="`static-${static}`" :data-icon-position="iconPosition ?? 'none'" :data-variant="variant ?? 'primary'" :type="submit ? 'submit' : 'button'" v-else><slot /></button>
 </template>
 
 <style scoped lang="sass">
@@ -24,6 +25,12 @@ defineProps<Props>();
 
   &[data-variant="primary"]
     @apply before:bg-transparent before:transition-colors transition-colors bg-woodsmoke-950 dark:bg-woodsmoke-0 text-woodsmoke-0 dark:text-woodsmoke-950 hover:text-woodsmoke-950 hover:before:bg-woodsmoke-0 dark:hover:text-woodsmoke-0 dark:hover:before:bg-woodsmoke-950
+
+    &.static-light
+      @apply bg-woodsmoke-950 text-woodsmoke-0
+    &.static-dark
+      @apply bg-woodsmoke-0 text-woodsmoke-950
+
   &[data-variant="secondary"]
     @apply bg-gradient-to-br from-woodsmoke-100 dark:from-woodsmoke-800 to-woodsmoke-0 dark:to-woodsmoke-950 before:bg-woodsmoke-0 dark:before:bg-woodsmoke-950 after:absolute after:h-4 after:w-[60%] after:rounded-full after:blur-xl after:transition-transform after:left-[50%] after:translate-x-[-50%] after:translate-y-3 after:top-[100%] after:bg-woodsmoke-200 dark:after:bg-woodsmoke-500 hover:after:translate-y-0
 
