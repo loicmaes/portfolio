5b3b744a67046df957fc07f07a5158cc28ece252
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-05-02 13:15:58 +0200

    :rocket: New chevron icon components

--- components/uikit/icons/arrows/IconChevronLeft.vue
+++ components/uikit/icons/arrows/IconChevronLeft.vue
@@ -4,7 +4,7 @@
 
 <template>
   <svg class="icon" viewBox="0 0 28 27" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M12.875 10.125L16.25 13.5L12.875 16.875" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M15.125 16.875L11.75 13.5L15.125 10.125" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
   </svg>
 </template>
 
