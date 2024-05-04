390d75468386adecfa44998b30f93232455ec06c
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-05-04 11:04:40 +0200

    :rocket: Add new two sliders icon component to icon library

---
+++ components/uikit/icons/search/IconTwoSliders.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <svg class="icon" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M14 15.5H21M3 15.5H5M5 15.5C5 16.8807 6.11929 18 7.5 18C8.88071 18 10 16.8807 10 15.5C10 14.1193 8.88071 13 7.5 13C6.11929 13 5 14.1193 5 15.5ZM20 9.5H21M3 9.5H10M16.5 12C15.1193 12 14 10.8807 14 9.5C14 8.11929 15.1193 7 16.5 7C17.8807 7 19 8.11929 19 9.5C19 10.8807 17.8807 12 16.5 12Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+</template>
+
+<style scoped lang="sass">
+
+</style>
