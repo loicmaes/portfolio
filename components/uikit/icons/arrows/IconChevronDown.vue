d82cdb592308c63219d7f3778ca961e833d13395
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-05-04 09:43:19 +0200

    :rocket: New chevron down icon component

---
+++ components/uikit/icons/arrows/IconChevronDown.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <svg class="icon" viewBox="0 0 28 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M17.375 12.375L14 15.75L10.625 12.375" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+</template>
+
+<style scoped lang="sass">
+
+</style>
