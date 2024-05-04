dcb1ed13dc68ddf29039ff2945b32c3534aacf89
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-05-04 17:28:58 +0200

    :sparkles: Display default project watcher layout

--- pages/projects/watch/[uuid].vue
+++ pages/projects/watch/[uuid].vue
@@ -8,17 +8,17 @@ definePageMeta({
   layout: 'project-watcher'
 });
 
-const project = projects.find(({ uuid }) => uuid === useRoute().params.uuid)
-const date    = useDateFormat(project?.createdAt || new Date(), {
+const { name, description, tags, client, thumbnail, uuid, createdAt } = projects.find(({ uuid }) => uuid === useRoute().params.uuid) || projects[0];
+const date    = useDateFormat(createdAt || new Date(), {
   format: DateFormat.SHORT_TEXT
-})
+});
 
 useHead({
-  title: `Loïc MAES · ${project?.name}`,
+  title: `Loïc MAES · ${name}`,
   meta: [
     {
       name: 'description',
-      content: project?.description,
+      content: description,
     },
     {
       name: 'author',
@@ -26,34 +26,92 @@ useHead({
     },
     {
       name: 'keywords',
-      content: project?.tags.join(', ')
+      content: tags.join(', ')
     }
   ]
 });
 
-const back = () => useRouter().back()
+const back = () => useRouter().back();
+
+const scrolledLimit = 10;
+const scrolled = ref(false);
+const scroll   = ()  => {
+  const windowScrollTop = window.scrollY;
+  const docElementTop   = document.documentElement.scrollTop;
+
+  scrolled.value = windowScrollTop > scrolledLimit || docElementTop > scrolledLimit;
+};
+
+if (process.browser) {
+  window.addEventListener('scroll', scroll);
+  window.addEventListener('load', scroll);
+}
 </script>
 
 <template>
-  <div data-page class="project">
-    <header class="project__header">
+  <article data-page class="project">
+    <header class="project__header" :class="{ 'scrolled': scrolled }">
       <button type="button" class="project__header--back" @click="back">
         <IconArrowLeft />
       </button>
 
       <p class="project__header--date">Créer le {{ date }}</p>
     </header>
-  </div>
+
+    <main class="project--wrapper">
+      <img :src="thumbnail.src" :alt="thumbnail.alt ?? ''" ref="thumbnailRef" class="project--thumbnail">
+
+      <div class="project--container">
+        <section class="project__section head">
+          <ul class="head__tags">
+            <li class="head__tags--item" :key="`tag-${index}`" v-for="(tag, index) in tags">{{ tag }}</li>
+          </ul>
+
+          <h1 class="head--title">{{ name }}</h1>
+          <p class="head--description">{{ description }}</p>
+        </section>
+
+        <hr class="project--separator" />
+
+        <section class="project__section outline outline-red-500 p-12"></section>
+      </div>
+    </main>
+  </article>
 </template>
 
 <style scoped lang="sass">
 .project
   &__header
-    @apply sticky top-0 z-10 flex items-center justify-between px-8 py-4
+    @apply sticky top-0 inset-x-0 z-10 flex items-center justify-between px-8 py-4 transition-colors bg-woodsmoke-0 dark:bg-woodsmoke-950
 
     &--back
       @apply aspect-square p-3
 
     &--date
       @apply text-woodsmoke-300 dark:text-woodsmoke-600
+
+  &--wrapper
+    @apply flex flex-col items-center
+
+  &--container
+    @apply relative px-8 py-12 lg:px-12 w-full max-w-[1024px] flex flex-col gap-16
+
+  &--thumbnail
+    @apply h-52 md:h-64 lg:h-96 w-full object-cover object-center
+
+  &--separator
+    @apply border-woodsmoke-200 dark:border-woodsmoke-400
+
+.head
+  &__tags
+    @apply mt-4 flex flex-wrap gap-2
+
+    &--item
+      @apply text-xs text-woodsmoke-300 py-2 px-3 bg-woodsmoke-100 dark:bg-woodsmoke-800 rounded-lg
+
+  &--title
+    @apply w-max max-w-full pt-8 pb-4 text-4xl lg:text-5xl 2xl:text-6xl font-black bg-gradient-to-r from-woodsmoke-950 dark:from-woodsmoke-0 to-woodsmoke-500 dark:to-woodsmoke-400 text-transparent bg-clip-text
+
+  &--description
+    @apply text-woodsmoke-500 dark:text-woodsmoke-500
 </style>
