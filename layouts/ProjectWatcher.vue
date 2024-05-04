077a885287555f74e16207477e0e230f25b648eb
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-05-04 11:52:18 +0200

    :rocket: Project watcher layout

---
+++ layouts/ProjectWatcher.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import BottomMenu from "~/components/globals/BottomMenu.vue";
+import ThemeSelector from "~/components/uikit/ThemeSelector.vue";
+</script>
+
+<template>
+  <div data-layout>
+    <slot />
+
+    <ThemeSelector />
+    <BottomMenu />
+  </div>
+</template>
+
+<style scoped lang="sass">
+
+</style>
