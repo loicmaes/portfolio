8f201334fd56cd661b46f903ca5add567f81264f
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-05-06 16:28:55 +0200

    :rocket: Add section title

--- components/locals/watcher/SectionWrapper.vue
+++ components/locals/watcher/SectionWrapper.vue
@@ -10,11 +10,16 @@ defineProps<Props>();
 
 <template>
   <section class="section-wrapper" v-if="section.children.length">
+    <h2 class="section-title" v-if="section.title">{{ section.title }}</h2>
     <SectionChild :child="child" v-for="(child, index) in section.children" :key="`${section.name.substring(0, Math.max(4, section.name.length))}-${index}`" />
   </section>
 </template>
 
 <style scoped lang="sass">
-.section-wrapper
-  @apply flex flex-col gap-6
+.section
+  &-wrapper
+    @apply flex flex-col gap-6
+
+  &-title
+    @apply text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-extrabold
 </style>
