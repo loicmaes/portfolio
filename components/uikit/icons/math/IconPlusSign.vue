7f98ad655f44c6ed47d1bdcc6ddb18ffa16cc788
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-05-06 02:38:30 +0200

    :rocket: New icons

---
+++ components/uikit/icons/math/IconPlusSign.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <svg class="icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M6 12H12M12 12H18M12 12V18M12 12V6" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+</template>
+
+<style scoped lang="sass">
+
+</style>
