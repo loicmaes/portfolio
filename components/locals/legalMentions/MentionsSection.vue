45fae4e4584654e8525684d9f4e8cee3bbd4a2a8
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-05-06 12:25:29 +0200

    :rocket: New section component

---
+++ components/locals/legalMentions/MentionsSection.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+interface Props {
+  name: string;
+}
+defineProps<Props>();
+</script>
+
+<template>
+  <section :id="name" class="section">
+    <h2 class="section--title">
+      <slot name="title" />
+    </h2>
+
+    <div class="section--wrapper">
+      <slot name="content" />
+    </div>
+  </section>
+</template>
+
+<style scoped lang="sass">
+.section
+  @apply relative w-full max-w-[768px] flex flex-col gap-4
+
+  &--title
+    @apply text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-extrabold
+
+  &--wrapper
+    @apply flex flex-col gap-4 text-justify text-woodsmoke-500 dark:text-woodsmoke-400
+</style>
