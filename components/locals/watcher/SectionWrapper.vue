b45cfd3c0bf9eadfed68c34731b7c5af692f4c54
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-05-06 10:27:28 +0200

    :rocket: Setup new working system watcher sections

---
+++ components/locals/watcher/SectionWrapper.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type {Section as T_Section} from "assets/types/projectContent.types";
+import SectionChild from "~/components/locals/watcher/SectionChild.vue";
+
+interface Props {
+  section: T_Section;
+}
+defineProps<Props>();
+</script>
+
+<template>
+  <section class="section-wrapper" v-if="section.children.length">
+    <SectionChild :child="child" v-for="(child, index) in section.children" :key="`${section.name.substring(0, Math.max(4, section.name.length))}-${index}`" />
+  </section>
+</template>
+
+<style scoped lang="sass">
+.section-wrapper
+  @apply flex flex-col gap-6
+</style>
